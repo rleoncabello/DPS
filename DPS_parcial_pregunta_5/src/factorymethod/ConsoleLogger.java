d9cbd12c880d6c331276d12b47f7afefd33a54b2
POST-GRADO <POST-GRADO@F20LAB213-05E>
2017-10-14 10:34:10 -0500

    se actualiza mensaje

--- DPS_parcial_pregunta_5/src/factorymethod/ConsoleLogger.java
+++ DPS_parcial_pregunta_5/src/factorymethod/ConsoleLogger.java
@@ -2,6 +2,6 @@ package factorymethod;
 
 public class ConsoleLogger implements Logger {
     public void log(String message) {
-        System.out.println("Console Logger: " + message);
+        System.out.println("Console Logger : " + message);
     }
 }
